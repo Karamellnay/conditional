e55f249d3f93c1f6b95441081e112b5ce98be916
Alexandra Zhukova <alexandrazhukova2010@mail.ru>
2023-10-04 11:07:37 +0300

    Two commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-int bilet = 15_000;
-int bonus = bilet / 20;
+        int ticket = 15_000;
+        int bonus = ticket / 20;
         System.out.println(bonus);
     }
 }
